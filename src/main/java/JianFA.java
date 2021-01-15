f1d87fa51f9a374955e390cafa70c8a2b3fa4096
dadahh <1059096576@qq.com>
2021-01-16 00:53:08 +0800

    增加黑桌子

---
+++ src/main/java/JianFA.java
@@ -0,0 +1,7 @@
+public class JianFA {
+    public static void main(String[] args) {
+        System.out.println("this is black desk");
+        System.out.println("this is black desk");
+        System.out.println("this is black desk");
+    }
+}
